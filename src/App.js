c96f78836990620f82f88dcdd1e34f5a74ce2fd5
anaberia <ana.beria3@gmail.com>
2022-10-04 16:34:07 -0700

    added states and fetch api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import react, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setvalue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading</h1>
+      </section>
+    );
+  }
+
+  return <h2>Jobs</h2>;
 }
 
 export default App;
